9ad7e61160291bf42e714504a4c462501a75c043
Rochelle Roberts and Uriel Gonzalez <rochelle+uriel@>
2019-08-05 11:30:37 -0700

    added to_do_list_test database and tests

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -42,4 +42,9 @@ namespace ToDoList
 
         }
     }
+    public static class DBConfiguration
+    {
+        public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
+    }
+
 }
